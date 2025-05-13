0ddfb17513ac514f43fe9f6391a02008f65b17fd
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-05-13 14:25:33 +0330

    add example for listactiveprojects, currencies, budgets and timezones

--- example/main.go
+++ example/main.go
@@ -2,203 +2,105 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	freelancer "github.com/shahinrahimi/go-freelancer-sdk/freelancer/v1"
 )
 
-func quickExample() {
-	// create freelancer client
-	c := freelancer.NewClient("YOUR-ACCESS-TOKEN")
+// set freelancer api access token from env or set it manulay
+var apiAccessToken = os.Getenv("FREELANCER_ACCESS_TOKEN")
 
+// create freelancer client with access token
+var client = freelancer.NewClient(apiAccessToken)
+
+func ListActiveProjects() {
 	// create active project service
-	s := c.NewListActiveProjectsService()
-
-	// get results
-	_, err := s.Do(context.Background())
-
-	// basic error handler
+	s := client.NewListActiveProjectsService()
+	// fetch result
+	resp, err := s.Do(context.Background())
+	// hanlde error
 	if err != nil {
-		log.Printf("error: %v", err)
+		fmt.Printf("error: %v", err)
 	}
 
-	// loop through projects
-	// for index, p := range res.Result.Projects {
-	// 	log.Printf("Project-%d\tTitle: ", index, p.Title)
-	// }
+	// use easily typed response
+	for index, p := range resp.Result.Projects {
+		timeSubmited := time.Unix(p.TimeSubmitted, 0).Format("2006-01-02 15:04:05")
+		fmt.Printf("Project-%03d\tID-%d\tAt: %s\tTitle: %s\n", index, p.ID, timeSubmited, p.Title)
+	}
 }
 
-func quickExample2() {
-	// create freelancer client
-	c := freelancer.NewClient("YOUR-ACCESS-TOKEN")
-
+func ListActiveProjectsWithMoreOptions() {
 	// create active project service
-	s := c.NewListActiveProjectsService()
-
-	// list projects base on query
-	s.SetQuery("Go Python")
-
-	// filter projects base on type
+	s := client.NewListActiveProjectsService()
+	// exlude projects base on the query
+	// query with space seprated
+	s.SetQuery("go python")
+	// exclude projects with type
 	s.SetProjectTypes([]freelancer.ProjectType{freelancer.ProjectTypeFixed})
-
-	// include projects full description
+	// include full Description
 	s.SetFullDescription(true)
+	// fetch result
+	resp, err := s.Do(context.Background())
 
-	// get results
-	res, err := s.Do(context.Background())
-
-	// basic error handler
+	// hanlde error
 	if err != nil {
 		log.Printf("error: %v", err)
 	}
 
-	// loop through projects
-	for index, p := range res.Result.Projects {
-		log.Printf("Project-%d\ttype: \nDesctiption: %s", index, p.Title, p.Description)
+	// use easily typed response
+	for i, p := range resp.Result.Projects {
+		timeSubmited := time.Unix(p.TimeSubmitted, 0).Format("2006-01-02 15:04:05")
+		fmt.Printf("Project-%03d\tID-%d\tAt: %s\tSeoUrl: %s\n", i, p.ID, timeSubmited, p.SeoURL)
 	}
 
+}
+
+func ListBudgets() {
+	s := client.NewListBudgetsService()
+
+	resp, err := s.Do(context.Background())
+
+	// handle erro
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+
+	for i, b := range resp.Result.Budgets {
+		fmt.Printf("Budget-%03d\tName: %s\tProjectType:%s CurrencyID:%d Minimum:%f Maximum:%f\n", i, b.Name, b.ProjectType, b.CurrencyID, b.Maximum, b.Minimum)
+	}
+}
+
+func ListCurrencies() {
+	s := client.NewListCurrenciesService()
+	resp, err := s.Do(context.Background())
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+	for i, c := range resp.Result.Currencies {
+		fmt.Printf("Currency-%03d\tID-%d\tName: %s\tCode: %s\tCountry:%s\tSign:%s\n", i, c.ID, c.Name, c.Code, c.Country, c.Sign)
+	}
+}
+
+func ListTimeZones() {
+	s := client.NewListTimezonesService()
+	resp, err := s.Do(context.Background())
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+	for i, t := range resp.Result.Timezones {
+		fmt.Printf("Timezone-%03d\t Timezone: %v\t ID: %d\tCountry: %s\tOffset: %f\n", i, t.Timezone, t.ID, t.Country, t.Offset)
+	}
 }
 
 func main() {
-	quickExample()
-}
-
-func main2() {
-
-	// load .env file
-	if err := godotenv.Load(); err != nil {
-		panic(err)
-	}
-
-	// get freelancer token
-	token := os.Getenv("FREELANCER_ACCESS_TOKEN")
-	if token == "" {
-		panic("FREELANCER_ACCESS_TOKEN is not set")
-	}
-	// create freelancer client
-	c := freelancer.NewClient(token)
-
-	// set BaseUrl main or sandbox freelancer.con api
-	c.SetBaseUrl(freelancer.BaseAPIMainURL)
-
-	// list active projects
-	pas := c.NewListActiveProjectsService()
-
-	pas.SetQuery("python json")
-	pas.SetProjectTypes([]freelancer.ProjectType{freelancer.ProjectTypeFixed})
-	pas.SetProjectUpgrades([]freelancer.ProjectUpgradeType{freelancer.ProjectUpgradeTypeSealed})
-	pas.SetFullDescription(true)
-	pas.SetUserCountryDetails(true)
-	pas.SetUserLocationDetails(true)
-	res, err := pas.Do(context.Background())
-	if err != nil {
-		if handledError, ok := err.(*freelancer.APIError2); ok {
-			log.Printf("Handled error: %s", handledError)
-			return
-		}
-		log.Printf("Unhandled Error: %s", err)
-	}
-	ownersID := make([]int, 0)
-	projectsID := make([]int, 0)
-	for pr, p := range res.Result.Projects {
-		log.Printf("Project #%d: %s", pr, p.Title)
-		log.Printf("Project #%d: %s", pr, p.SeoURL)
-		//log.Printf("Project #%d: %s", pr, p.Description)
-		log.Printf("Project #%d: %s", pr, p.PreviewDesc)
-		log.Printf("Project #%d: %s", pr, p.Location.City)
-		ownersID = append(ownersID, p.OwnerID)
-		projectsID = append(projectsID, p.ID)
-	}
-
-	us := c.NewListUsersService()
-	us.SetUsers(ownersID)
-	us.SetAvatar(true)
-	res2, err := us.Do(context.Background())
-	if err != nil {
-		if handledError, ok := err.(*freelancer.APIError2); ok {
-			log.Printf("Handled error: %s", handledError)
-			return
-		}
-		log.Printf("Unhandled Error: %s", err)
-		return
-	}
-	for pr, p := range res2.Result.Users {
-		log.Printf("User #%s: %s", pr, p.Username)
-		log.Printf("User #%s: %s", pr, p.Avatar)
-	}
-
-	res3, err := c.NewListBudgetsService().Do(context.Background())
-	if err != nil {
-		if handledError, ok := err.(*freelancer.APIError2); ok {
-			log.Printf("Handled error: %s", handledError)
-			return
-		}
-		log.Printf("Unhandled Error: %s", err)
-		return
-	}
-	for pr, p := range res3.Result.Budgets {
-		log.Printf("Budget #%d with minimum: %f and maximum: %f , name: %s and project type: %s", pr, p.Minimum, p.Maximum, p.Name, p.ProjectType)
-	}
-
-	res4, err := c.NewListCountriesService().Do(context.Background())
-	if err != nil {
-		if handledError, ok := err.(*freelancer.APIError2); ok {
-			log.Printf("Handled error: %s", handledError)
-			return
-		}
-		log.Printf("Unhandled Error: %s", err)
-		return
-	}
-	for pr, p := range res4.Result.Countries {
-		log.Printf("Country #%d: %s, %s", pr, p.Name, p.Code)
-	}
-
-	res5, err := c.NewListCategoriesService().Do(context.Background())
-	if err != nil {
-		if handledError, ok := err.(*freelancer.APIError2); ok {
-			log.Printf("Handled error: %s", handledError)
-			return
-		}
-		log.Printf("Unhandled Error: %s", err)
-		return
-	}
-	for pr, p := range res5.Result.Categories {
-		log.Printf("Category #%d: %s", pr, p.Name)
-	}
-
-	ps := c.NewListProjectsService()
-	ps.SetProjects(projectsID)
-	ps.SetLocationDetails(true)
-	ps.SetUserLocationDetails(true)
-	res6, err := ps.Do(context.Background())
-	if err != nil {
-		if handledError, ok := err.(*freelancer.APIError2); ok {
-			log.Printf("Handled error: %s", handledError)
-			return
-		}
-		log.Printf("Unhandled Error: %s", err)
-		return
-	}
-	for pr, p := range res6.Result.Projects {
-		log.Printf("Project #%d: %s", pr, p.Title)
-		log.Printf("Project #%d: %s", pr, p.SeoURL)
-		//log.Printf("Project #%d: %s", pr, p.Description)
-		log.Printf("Project #%d: %s", pr, p.Location.City)
-	}
-
-	res7, err := c.NewListCurrenciesService().Do(context.Background())
-	if err != nil {
-		if handledError, ok := err.(*freelancer.APIError2); ok {
-			log.Printf("Handled error: %s", handledError)
-			return
-		}
-		log.Printf("Unhandled Error: %s", err)
-		return
-	}
-	for pr, p := range res7.Result.Currencies {
-		log.Printf("Currency #%d: %s", pr, p.Name)
-	}
-
+	ListTimeZones()
+	// ListCurrencies()
+	// ListBudgets()
+	// ListActiveProjects()
+	// ListActiveProjectsWithMoreOptions()
 }
