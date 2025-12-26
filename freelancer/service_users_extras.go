95f8de87c399955dbcb8dc10f34005b9db1a4e01
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2025-12-26 11:37:55 +0330

    refactor services related to users extras

--- freelancer/service_users_extras.go
+++ freelancer/service_users_extras.go
@@ -1,234 +1,95 @@
 package freelancer
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"net/http"
+	"net/url"
 )
 
-type listReputations struct {
-	client       *Client
-	users        []int64
-	jobs         []int32
-	role         *RoleType
-	jobHistory   *bool
-	projectStats *bool
-	rehireRates  *bool
+type ListReputationsOptions struct {
+	Users        []int64
+	Jobs         []int64
+	Role         *RoleType
+	JobHistory   *bool
+	ProjectStats *bool
+	RehireRates  *bool
 }
 
-func (s *listReputations) Do(ctx context.Context) (*ListUsersReputationsResponse, error) {
-	r := &request{
-		method:   http.MethodGet,
-		endpoint: string(UsersReputations),
+// Gets the reputations for a list of users.
+// It maps to the `GET` `/users/0.1/reputations` endpoint.
+func (s *UsersExtrasService) ListReputations(ctx context.Context, opts *ListReputationsOptions) (*ListUsersReputationsResponse, error) {
+	query := url.Values{}
+	if opts != nil {
+		for _, val := range opts.Users {
+			addInt64(query, "users[]", &val)
+		}
+		for _, val := range opts.Jobs {
+			addInt64(query, "jobs[]", &val)
+		}
+		addEnum(query, "role", opts.Role)
+		addBool(query, "job_history", opts.JobHistory)
+		addBool(query, "project_stats", opts.ProjectStats)
+		addBool(query, "rehire_rates", opts.RehireRates)
 	}
-
-	for _, id := range s.users {
-		r.addParam("users[]", id)
-	}
-	for _, id := range s.jobs {
-		r.addParam("jobs[]", id)
-	}
-	if s.role != nil {
-		r.addParam("role", *s.role)
-	}
-	if s.jobHistory != nil {
-		r.addParam("job_history", *s.jobHistory)
-	}
-	if s.projectStats != nil {
-		r.addParam("project_stats", *s.projectStats)
-	}
-	if s.rehireRates != nil {
-		r.addParam("rehire_rates", *s.rehireRates)
-	}
-	return execute[*ListUsersReputationsResponse](ctx, s.client, r)
-
+	return execute[*ListUsersReputationsResponse](ctx, s.client, http.MethodGet, string(UsersReputations), query, nil)
 }
 
-// SetUsers sets the filter user IDs for the reputation list service.
-func (s *listReputations) SetUsers(users []int64) *listReputations {
-	s.users = users
-	return s
-}
-
-// SetJobs limits users to a set of job IDs as a filter.
-func (s *listReputations) SetJobs(jobs []int32) *listReputations {
-	s.jobs = jobs
-	return s
-}
-
-// SetRole sets the type of reputation filter (e.g., freelancer or employer).
-func (s *listReputations) SetRole(val RoleType) *listReputations {
-	s.role = &val
-	return s
-}
-
-// SetJobHistory sets whether to return job history projection for each user.
-func (s *listReputations) SetJobHistory(val bool) *listReputations {
-	s.jobHistory = &val
-	return s
-}
-
-// SetProjectState sets whether to return project stats projection for each user.
-func (s *listReputations) SetProjectState(val bool) *listReputations {
-	s.projectStats = &val
-	return s
-}
-
-// SetRehireRates sets whether to return rehire rates projection for a single freelancer.
-func (s *listReputations) SetRehireRates(val bool) *listReputations {
-	s.rehireRates = &val
-	return s
-}
-
-type listEnterprises struct {
-	client        *Client
-	enterprises   []int
-	internalNames []string
-	names         []string
-	seoUrls       []string
-	userID        *int64
-	ignoreTest    *bool
-	limit         *int
-	offset        *int
+type ListEnterprisesOptions struct {
+	Enterprises   []int64
+	InternalNames []string
+	Names         []string
+	SeoUrls       []string
+	UserID        *int64
+	IgnoreTest    *bool
+	Limit         *int
+	Offset        *int
 }
 
 // TODO: refine with typed response
-func (s *listEnterprises) Do(ctx context.Context) (*RawResponse, error) {
-	r := &request{
-		method:   http.MethodGet,
-		endpoint: string(UsersEnterprises),
+
+// Returns a list of enterprises.
+// It maps to the `GET` `/users/0.1/enterprises` endpoint.
+func (s *UsersExtrasService) ListEnterprises(ctx context.Context, opts *ListEnterprisesOptions) (*RawResponse, error) {
+	query := url.Values{}
+	if opts != nil {
+		for _, val := range opts.Enterprises {
+			addInt64(query, "enterprises[]", &val)
+		}
+		for _, val := range opts.InternalNames {
+			addString(query, "internal_names[]", &val)
+		}
+		for _, val := range opts.Names {
+			addString(query, "names[]", &val)
+		}
+		for _, val := range opts.SeoUrls {
+			addString(query, "seo_urls[]", &val)
+		}
+		addInt64(query, "user_id", opts.UserID)
+		addBool(query, "ignore_test", opts.IgnoreTest)
+		addInt(query, "limit", opts.Limit)
+		addInt(query, "offset", opts.Offset)
 	}
+	return execute[*RawResponse](ctx, s.client, http.MethodGet, string(UsersEnterprises), query, nil)
+}
 
-	for _, val := range s.enterprises {
-		r.addParam("enterprises[]", val)
+type ListPortfoliosOptions struct {
+	Users  []int64
+	Limit  *int
+	Offset *int
+}
+
+// The service gets the portfolios for a list of users
+// Returns a list of portfolios of users. Number of portfolios of all users can be limited by the offset and limit parameters.
+func (s *UsersExtrasService) ListPortfolios(ctx context.Context, opts *ListPortfoliosOptions) (*ListUsersPortfoliosResponse, error) {
+	query := url.Values{}
+	if opts != nil {
+		for _, val := range opts.Users {
+			addInt64(query, "users[]", &val)
+		}
+		addInt(query, "limit", opts.Limit)
+		addInt(query, "offset", opts.Offset)
 	}
-
-	for _, val := range s.internalNames {
-		r.addParam("internal_names[]", val)
-	}
-
-	for _, val := range s.names {
-		r.addParam("names[]", val)
-	}
-
-	for _, val := range s.seoUrls {
-		r.addParam("seo_urls[]", val)
-	}
-
-	if s.userID != nil {
-		r.addParam("user_id", *s.userID)
-	}
-
-	if s.ignoreTest != nil {
-		r.addParam("ignore_test", *s.ignoreTest)
-	}
-
-	if s.limit != nil {
-		r.addParam("limit", *s.limit)
-	}
-
-	if s.offset != nil {
-		r.addParam("offset", *s.offset)
-	}
-	return execute[*RawResponse](ctx, s.client, r)
-}
-
-// SetEnterprises sets the filter to return enterprises with the specified enterprise IDs.
-func (s *listEnterprises) SetEnterprises(vals []int) *listEnterprises {
-	s.enterprises = vals
-	return s
-}
-
-// SetInternalNames sets the filter to return enterprises with the specified internal names.
-func (s *listEnterprises) SetInternalNames(vals []string) *listEnterprises {
-	s.internalNames = vals
-	return s
-}
-
-// SetNames sets the filter to return enterprises with the specified names.
-func (s *listEnterprises) SetNames(vals []string) *listEnterprises {
-	s.names = vals
-	return s
-}
-
-// SetSeoUrls sets the filter to return enterprises with the specified SEO URLs.
-func (s *listEnterprises) SetSeoUrls(vals []string) *listEnterprises {
-	s.seoUrls = vals
-	return s
-}
-
-// SetUserID sets the filter to return enterprises for the specified user ID.
-func (s *listEnterprises) SetUserID(val int64) *listEnterprises {
-	s.userID = &val
-	return s
-}
-
-// SetIgnoreTest sets the filter to ignore test enterprises.
-func (s *listEnterprises) SetIgnoreTest(val bool) *listEnterprises {
-	s.ignoreTest = &val
-	return s
-}
-
-// SetLimit sets the maximum number of results to return. Default is 100.
-func (s *listEnterprises) SetLimit(val int) *listEnterprises {
-	s.limit = &val
-	return s
-}
-
-// SetOffset sets the number of results to skip, for pagination. Default is 0.
-func (s *listEnterprises) SetOffset(val int) *listEnterprises {
-	s.offset = &val
-	return s
-}
-
-type listPortfolios struct {
-	client *Client
-	users  []int64
-	limit  *int
-	offset *int
-}
-
-func (s *listPortfolios) DO(ctx context.Context) (*ListUsersPortfoliosResponse, error) {
-	r := &request{
-		method:   http.MethodGet,
-		endpoint: string(UsersPortfolios),
-	}
-
-	for id := range s.users {
-		r.addParam("users[]", id)
-	}
-
-	if s.limit != nil {
-		r.addParam("limit", s.limit)
-	}
-
-	if s.offset != nil {
-		r.addParam("offset", s.offset)
-	}
-	return execute[*ListUsersPortfoliosResponse](ctx, s.client, r)
-}
-
-// Returns portfolios of specified user ids
-func (s *listPortfolios) SetUsers(users []int64) *listPortfolios {
-	s.users = users
-	return s
-}
-
-// Number of portfolios required for all users.
-func (s *listPortfolios) SetLimit(limit int) *listPortfolios {
-	s.limit = &limit
-	return s
-}
-
-// starting offset of portfolios for each user
-func (s *listPortfolios) SetOffset(offset int) *listPortfolios {
-	s.offset = &offset
-	return s
-}
-
-type createViolation struct {
-	client *Client
+	return execute[*ListUsersPortfoliosResponse](ctx, s.client, http.MethodGet, string(UsersPortfolios), query, nil)
 }
 
 type CreateViolationBody struct {
@@ -241,100 +102,42 @@ type CreateViolationBody struct {
 	Url              string                    `json:"url,omitempty"`
 }
 
-func (s *createViolation) Do(ctx context.Context, b CreateViolationBody) (*RawResponse, error) {
-	m, err := json.Marshal(b)
-	if err != nil {
-		return nil, err
-	}
-
-	r := &request{
-		method:   http.MethodPost,
-		endpoint: string(UsersViolationReports),
-		body:     bytes.NewBuffer(m),
-	}
-	return execute[*RawResponse](ctx, s.client, r)
+// Creates a user violation report.
+// It maps to the `POST` `/users/0.1/violation_reports` endpoint.
+func (s *UsersExtrasService) CreateViolation(ctx context.Context, b CreateViolationBody) (*RawResponse, error) {
+	return execute[*RawResponse](ctx, s.client, http.MethodPost, string(UsersViolationReports), nil, b)
 }
 
-type listPools struct {
-	client          *Client
-	pools           []int
-	names           []string
-	seoUrls         []string
-	ignoreTest      *bool
-	isTalentNetwork *bool
-	limit           *int
-	offset          *int
+type ListPoolsOptions struct {
+	Pools           []int
+	Names           []string
+	SeoUrls         []string
+	IgnoreTest      *bool
+	IsTalentNetwork *bool
+	Limit           *int
+	Offset          *int
 }
 
 // TODO: refine with typed response
-func (s *listPools) Do(ctx context.Context) (*RawResponse, error) {
-	r := &request{
-		method:   http.MethodGet,
-		endpoint: string(UsersPools),
-	}
 
-	for _, val := range s.pools {
-		r.addParam("pools[]", val)
+// Returns a list of pools belonging to the current user.
+// It maps to the `GET` `/users/0.1/pools` endpoint.
+func (s *UsersExtrasService) ListPools(ctx context.Context, opts *ListPoolsOptions) (*RawResponse, error) {
+	query := url.Values{}
+	if opts != nil {
+		for _, val := range opts.Pools {
+			addInt(query, "pools[]", &val)
+		}
+		for _, val := range opts.Names {
+			addString(query, "internal_names[]", &val)
+		}
+		for _, val := range opts.SeoUrls {
+			addString(query, "seo_urls[]", &val)
+		}
+		addBool(query, "ignore_test", opts.IgnoreTest)
+		addBool(query, "is_talent_network", opts.IsTalentNetwork)
+		addInt(query, "limit", opts.Limit)
+		addInt(query, "offset", opts.Offset)
 	}
-	for _, val := range s.names {
-		r.addParam("internal_names[]", val)
-	}
-	for _, val := range s.seoUrls {
-		r.addParam("seo_urls[]", val)
-	}
-	if s.ignoreTest != nil {
-		r.addParam("ignore_test", *s.ignoreTest)
-	}
-	if s.isTalentNetwork != nil {
-		r.addParam("is_talent_network", *s.isTalentNetwork)
-	}
-	if s.limit != nil {
-		r.addParam("limit", *s.limit)
-	}
-	if s.offset != nil {
-		r.addParam("offset", *s.offset)
-	}
-	return execute[*RawResponse](ctx, s.client, r)
-}
-
-// SetPools sets the list of pool IDs to filter results by.
-func (s *listPools) SetPools(vals []int) *listPools {
-	s.pools = vals
-	return s
-}
-
-// SetNames sets the list of pool names to filter results by.
-func (s *listPools) SetNames(vals []string) *listPools {
-	s.names = vals
-	return s
-}
-
-// SetSeoUrls sets the list of SEO URLs to filter results by.
-func (s *listPools) SetSeoUrls(vals []string) *listPools {
-	s.seoUrls = vals
-	return s
-}
-
-// SetIgnoreTest sets whether to ignore test pools.
-func (s *listPools) SetIgnoreTest(val bool) *listPools {
-	s.ignoreTest = &val
-	return s
-}
-
-// SetIsTalentNetwork sets whether to filter pools based on the talent network flag.
-func (s *listPools) SetIsTalentNetwork(val bool) *listPools {
-	s.isTalentNetwork = &val
-	return s
-}
-
-// SetLimit sets the maximum number of results to return. Default is 100.
-func (s *listPools) SetLimit(val int) *listPools {
-	s.limit = &val
-	return s
-}
-
-// SetOffset sets the number of results to skip for pagination. Default is 0.
-func (s *listPools) SetOffset(val int) *listPools {
-	s.offset = &val
-	return s
+	return execute[*RawResponse](ctx, s.client, http.MethodGet, string(UsersPools), query, nil)
 }
